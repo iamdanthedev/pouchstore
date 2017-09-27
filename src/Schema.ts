a54304967d488308e21004c1f1e6a7fd1bf02d6a
Daniel Khoroshko <rasdaniil@gmail.com>
2017-09-27 15:15:18 +0300

    + Schema.FIND_PRIMARY + Schema.GET_TYPE refactored Schema.GET_DEFAULTS

--- src/Schema.ts
+++ src/Schema.ts
@@ -14,17 +14,98 @@ export class Schema<T extends ItemModel> {
   private _schema: JsonSchema<T>;
   private _validate: Ajv.ValidateFunction;
   private _defaults: Partial<T> = {};
+  private _primaryField: keyof T;
+  private _type: string;
+
 
   constructor(db: DB, schema: JsonSchema<T>) {
     this._db = db;
-    this._schema = schema;
+    this._schema = clone(schema);
     this._validate = this._db.$ajv.compile(this._schema);
 
-    this._extractDefaults();
+    // set and check primary field
+    this._primaryField = Schema.FIND_PRIMARY_KEY(this._schema);
+    if (!this._primaryField) {
+      throw new Error('pouchstore: schema has no primary key');
+    }
+
+    // set and check schema object type
+    const type: string | undefined = Schema.GET_TYPE(this._schema);
+    if (!type) {
+      throw new Error('pouchstore: schema has no type property');
+    }
+    else {
+      this._type = type;
+    }
+
+    // set defaults
+    this._defaults = Schema.GET_DEFAULTS(this._schema);
+  }
+
+  /**
+   * Finds a primary field of a schema and returns its key
+   *
+   * @param {JsonSchema<T extends ItemModel>} schema
+   * @returns {keyof T}
+   * @constructor
+   */
+  public static FIND_PRIMARY_KEY<T extends ItemModel>(schema: JsonSchema<T>): keyof T {
+    return Object.keys(schema.properties).find((prop: keyof T) => {
+      return schema.properties[prop].primary === true;
+    }) as keyof T;
+  }
+
+  /**
+   * Gets schema items' type from a provided schema
+   *
+   * @param {JsonSchema<T extends ItemModel>} schema
+   * @returns {string}
+   * @constructor
+   */
+  public static GET_TYPE<T extends ItemModel>(schema: JsonSchema<T>): string | undefined {
+    return schema.properties.type.const || schema.properties.type.default;
+  }
+
+  /**
+   * Saves a copy of schema defaults values
+   * @param schema
+   * @return {Partial<T extends ItemModel>}
+   */
+  public static GET_DEFAULTS<T extends ItemModel>(schema: JsonSchema<T>): Partial<T> {
+
+    if (!schema.properties || Object.keys(schema.properties).length === 0) {
+      return {};
+    }
+
+    const result: Partial<T> = {};
+
+    Object.keys(schema.properties).forEach((prop: keyof T) => {
+
+      const constValue = schema.properties[prop].const;
+      const defaultValue = schema.properties[prop].default;
+
+      if (defaultValue === undefined && constValue === undefined) {
+        return;
+      }
+
+      const value = constValue !== undefined ? constValue : defaultValue;
+
+      result[prop as keyof T] = clone(value);
+    });
+
+    return result;
   }
 
   get defaults(): Partial<T> {
     return clone(this._defaults);
+  }
+
+  get primaryField(): keyof T {
+    return this._primaryField;
+  }
+
+  get type(): string {
+    return this._type;
   }
 
   public validateDoc(doc: {}): doc is T {
@@ -55,32 +136,6 @@ export class Schema<T extends ItemModel> {
     else {
       throw new Error('Unknown error occured');
     }
-  }
-
-  /**
-   * Saves a copy of schema defaults values
-   * @private
-   */
-  private _extractDefaults(): void {
-    const schema: JsonSchema<T> = this._schema;
-
-    if (!schema.properties || Object.keys(schema.properties).length === 0) {
-      return;
-    }
-
-    Object.keys(schema.properties).map((prop: keyof T) => {
-
-      const constValue = schema.properties[prop].const;
-      const defaultValue = schema.properties[prop].default;
-
-      if (defaultValue === undefined && constValue === undefined) {
-        return;
-      }
-
-      const value = constValue !== undefined ? constValue : defaultValue;
-
-      this._defaults[prop as keyof T] = clone(value);
-    });
   }
 
 }

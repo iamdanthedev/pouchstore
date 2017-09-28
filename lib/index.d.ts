1093a35609eb612125ad7c8c50f8a8dc6bd3ef51
Daniel Khoroshko <rasdaniil@gmail.com>
2017-09-28 06:45:05 +0300

    fixing gitignore

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { DB } from './DB';
-export { Collection } from './Collection';
-export { Item, ItemModel } from './Item';
-export { CollectionOptions, ICollectionOptions } from './StoreOptions';
-export { NewItemDoc, ExistingItemDoc, ItemDoc, MapOf, Attachment, Attachments, WithAttachments, OnBeforeRemove } from './types';
--- lib/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import { ItemDoc, NewItemDoc } from './types';
-import { ItemModel } from './Item';
-export declare function isNil(arg: any): arg is undefined | null;
-export declare function isNewDocument<T extends ItemModel>(arg: ItemDoc<T>): arg is NewItemDoc<T>;
